8a5b77382af89a33e9527b032e31f559b40832af
Saumit <saumit2102@gmail.com>
2024-12-01 04:11:22 +0530

    bugfix fix navBar shrinking

--- components/NavLinks.tsx
+++ components/NavLinks.tsx
@@ -13,7 +13,7 @@ export const NavLinks = () => {
         { text: "Contact Us", url: "/contact-us" },
     ]
     return (<div className="flex gap-5 lg:gap-9 md:flex-row items-end flex-col mt-3">
-        {navLinks.map((link) => <Link key={"link__" + link.url} className={`text-xl lg:text-xl md:text-md  ${pathname === link.url ? "text-cms font-bold" : ""}`} href={link.url}>
+        {navLinks.map((link) => <Link key={"link__" + link.url} className={`text-xl lg:text-xl md:text-md hover:underline hover:text-cms underline-offset-4  ${pathname === link.url ? "text-cms font-bold" : ""}`} href={link.url}>
             <span>{link.text}</span>
         </Link>)}
     </div>)
