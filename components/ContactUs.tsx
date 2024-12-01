5ba01f93e8d3420641e34cd983685db0898a8b6c
gokhul-b <gokhulb2001@gmail.com>
2024-12-01 17:48:22 +0530

    carousel-02

--- components/ContactUs.tsx
+++ components/ContactUs.tsx
@@ -2,18 +2,18 @@ import Link from "next/link";
 import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faWhatsapp } from "@fortawesome/free-brands-svg-icons/faWhatsapp";
-import { faEnvelope } from "@fortawesome/free-solid-svg-icons";
+import { faEnvelope, faPhone } from "@fortawesome/free-solid-svg-icons";
 
 const ContactUs = () => {
   return (
-    <div>
-      <div className="flex items-start justify-center">
-        <div className="p-8 space-y-8">
+    <div className="mb-12 mx-6 lg:mx-32 md:mx-24 ">
+      <div className="flex flex-col items-start w-full space-y-6 lg:space-y-16">
+        <div className="lg:space-y-8 space-y-4">
           <p className="text-2xl">
-            <span className="lg:text-9xl text-6xl">Feel free</span> <br /> to
-            share your message with us, <br />
+            <span className="lg:text-8xl md:text-6xl text-4xl">Feel free</span>{" "}
+            <br /> to share your message with us, <br />
             and we will respond{" "}
-            <span className="lg:text-7xl text-4xl">as soon as possible...</span>
+            <span className="lg:text-6xl text-2xl">as soon as possible...</span>
           </p>
           <div>
             <Link
@@ -22,20 +22,41 @@ const ContactUs = () => {
             >
               <FontAwesomeIcon
                 icon={faWhatsapp}
-                className="text-white lg:w-12 w-6  h-full"
+                className="text-white lg:w-12 w-6 h-full"
               />
               <p className="lg:text-3xl text-xl text-slate-100">WhatsApp us</p>
             </Link>
           </div>
         </div>
-      </div>
-      <div>
-        <div>
+        <div className="w-full flex flex-col lg:flex-row gap-8 lg:gap-32">
+          <div className="flex">
+            <div className="border max-w-fit p-2 rounded-md max-h-fit">
+              <FontAwesomeIcon
+                icon={faEnvelope}
+                className="lg:w-8 md:w-6 w-4"
+              />
+            </div>
+            <div className="lg:ml-6 ml-3 lg:mt-4 mt-2">
+              <p className="font-semibold lg:text-xl">Chat to us</p>
+              <p className="text-gray-500">
+                Our friendly team team is here to help
+              </p>
+              <p className="font-medium">support@chillmountstays.com</p>
+            </div>
+          </div>
           <div>
-            <FontAwesomeIcon icon={faEnvelope} />
+            <div className="flex">
+              <div className="border max-w-fit p-2 rounded-md max-h-fit">
+                <FontAwesomeIcon icon={faPhone} className="lg:w-8 md:w-6 w-4" />
+              </div>
+              <div className="lg:ml-6 ml-3 lg:mt-4 mt-2">
+                <p className="font-semibold lg:text-xl">Call us</p>
+                <p className="text-gray-500">From day - To day, 8am to 10pm</p>
+                <p className="font-medium">+91 9842083815</p>
+              </div>
+            </div>
           </div>
         </div>
-        <div>call</div>
       </div>
     </div>
   );
