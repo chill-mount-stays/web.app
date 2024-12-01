5986775be529f7bc8bcac217db9776a004644424
Saumit <saumit2102@gmail.com>
2024-12-01 13:43:44 +0530

    bugfix BPTV card size changes

--- components/PlacesToVisit.tsx
+++ components/PlacesToVisit.tsx
@@ -15,12 +15,12 @@ export const PlacesToVisit = () => {
         url: "/assets/BPTV/roseGarden.jpg"
     }]
     return <div className="p-5 flex flex-col items-center lg:gap-14">
-        <p className="text-center text-2xl md:text-3xl font-bold mb-4">Best Places to Visit</p>
+        <p className="text-center text-2xl md:text-3xl font-semibold mb-4 font-scottel">Best Places to Visit</p>
         <div className="flex flex-col gap-5 justify-around lg:flex-row xl:gap-10">{placesToVisitDetails.map((image, index) => {
             return <motion.div initial={{ opacity: 0, y: 20 * index }} animate={{ y: 0, opacity: 1 }} className="flex flex-col items-center gap-3" key={image.name}>
                 <div
                     className="w-[300px] h-[180px] xl:w-[450px] xl:h-[250px] relative overflow-hidden rounded-3xl border-white border-4 border-solid drop-shadow-xl">
-                    <Image className="w-full h-full object-center absolute object-cover" width={100} height={100} alt={image.name} src={image.url} />
+                    <Image className="w-full h-full object-center absolute object-cover" width={1000} height={1000} alt={image.name} src={image.url} />
                 </div>
                 <p className="font-bold">{image.name}</p>
             </motion.div>
